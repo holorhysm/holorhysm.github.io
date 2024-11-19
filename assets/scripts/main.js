0cb0630014d605fe282d8eaf26747a75f2adcd7f
綾坂こと <148830788+AXT-AyaKoto@users.noreply.github.com>
2024-11-20 01:27:02 +0900

    雑スライドショー実装

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -32,3 +32,28 @@ const $$ = selector => Array.from(document.querySelectorAll(selector));
 /** ================================================================================================
  * 以下、本体
 ================================================================================================= */
+
+/* ======== #Top>div.slideshow : 指定画像群でスライドショー ======== */
+(() => {
+    const interval = 5000;
+    const fadeDuration = 500;
+    let currentIndex = -1;
+    const slideshowImagesPath = [
+        "/assets/images/screenshots/title.png",
+        "/assets/images/screenshots/session.png",
+    ];
+    const slideshowElement = $("#Top>div.slideshow");
+    const update = () => {
+        const nextIndex = (currentIndex + 1) % slideshowImagesPath.length;
+        const nextImagePath = slideshowImagesPath[nextIndex];
+        const nextImageElement = document.createElement("img");
+        nextImageElement.src = nextImagePath;
+        nextImageElement.style.opacity = 0;
+        slideshowElement.appendChild(nextImageElement);
+        nextImageElement.animate([{ opacity: 0 }, { opacity: 1, offset: fadeDuration / (interval + fadeDuration) }, { opacity: 1, offset: 1 }], { duration: interval + fadeDuration });
+        setTimeout(() => { slideshowElement.removeChild(nextImageElement); }, interval + fadeDuration);
+        currentIndex = nextIndex;
+        setTimeout(update, interval);
+    };
+    update();
+})();
